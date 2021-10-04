fb1ccf6f3b307eb6e5b5bf2533f6eeb631ac9007
yadi <yadivaladez@gmail.com>
2021-10-04 16:32:02 -0700

    add starting assets
    - added affirmation component to test font import
    - added initial font
    - added logo
    - starting folder structure

---
+++ src/components/DailyAffirmation/DailyAffirmation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './DailyAffirmation.module.scss';
+
+
+export const DailyAffirmation = (props) => {
+    return (
+        <h1 className={style["affirmation"]}>The Joy Room</h1>
+    )
+}
+
+export default DailyAffirmation;
