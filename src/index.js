9d5704bc62bff7bbfae74faff80f37e6df6bdb08
yadi <yadivaladez@gmail.com>
2021-10-08 14:32:53 -0700

    content card

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ import Header from './components/Header/Header';
 import Greeting from './components/Greeting/Greeting';
 import Affirmation from './components/DailyAffirmation/DailyAffirmation.js';
 import ContentCard from './components/ContentCard/ContentCard';
-
-
-
+import Footer from './components/Footer/Footer';
 import reportWebVitals from './reportWebVitals';
+
+const TestCard = {
+  eyebrow: 'OCTOBER WoC SPOTLIGHT'
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <Greeting/>
     <Affirmation/>
-    <ContentCard/>
+    <ContentCard eyebrow={TestCard.eyebrow}/>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -25,3 +28,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
