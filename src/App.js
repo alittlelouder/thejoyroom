0aad5729515a377f5483701c26955f43a5ea3575
John Brown <thisisjohbrown@gmail.com>
2021-10-24 11:38:19 -0700

    add preview functionality

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import CardContainer from "./components/CardContainer/CardContainer";
 import ContentCard from "./components/ContentCard/ContentCard";
 import Customizer from './components/Customizer/Customizer';
 import Footer from './components/Footer/Footer';
-
-const contentURL = 'https://graphql.contentful.com/content/v1/spaces/wr6ttoy5edxe/environments/master?access_token=Z2xqNSZWD1jhGfh7RtaxqdMyX4iOYT4NQ_2-g9EiVHI&query=query($preview:%20Boolean,%20$limit:%20Int,%20$contentDatesId:%20String!)%20{%20contentDates(preview:%20$preview,%20id:%20$contentDatesId)%20{%20affirmation%20{%20json%20}%20sectionsCollection%20{%20items%20{%20title%20description%20link%20linkText%20image%20{%20url%20width%20height%20}%20sectionName%20resourcesCollection(limit:%20$limit)%20{%20items%20{%20eyebrow%20title%20description%20externalLink%20externalLinkText%20image%20{%20url%20width%20height%20}%20}%20}%20}%20}%20}%20}%20&variables={%20%22contentDatesId%22:%20%222C9D1e8DdPI0vGT4XXR4OU%22,%20%22preview%22:%20null,%20%22limit%22:%206%20}';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    const params = new URLSearchParams(window.location.search)
+    let isPreview = params.get('preview') ? true : false;
+    let previewToken = 'Qg5GbDwKrU8Dz-eys6IP_dXhxIIt9an_6CPfzRWIcrI';
+    let liveToken = 'Z2xqNSZWD1jhGfh7RtaxqdMyX4iOYT4NQ_2-g9EiVHI';
 
     this.handleCustomizeClick = this.handleCustomizeClick.bind(this);
     this.handleGreetingChange = this.handleGreetingChange.bind(this);
@@ -47,22 +49,35 @@ class App extends React.Component {
     this.handleColorPaletteChange(this.state.palette);
     this.handleBackgroundChange(this.state.background);
 
+    
+
+    if (params.get('preview')) {
+      this.getContent(isPreview, params.get('preview'), previewToken)
+    } else {
+      fetch(`https://cdn.contentful.com/spaces/wr6ttoy5edxe/environments/master/entries?access_token=${liveToken}&content_type=contentDates&limit=1`)
+        .then(response => response.json())
+        .then(json => {
+          this.getContent(isPreview, json.items[0].sys.id, liveToken)
+        })
+    }
+  }
+
+  getContent(preview, id, token) {
+    const contentURL = `https://graphql.contentful.com/content/v1/spaces/wr6ttoy5edxe/environments/master?access_token=${token}&query=query($preview:%20Boolean,%20$limit:%20Int,%20$contentDatesId:%20String!)%20{%20contentDates(preview:%20$preview,%20id:%20$contentDatesId)%20{%20affirmation%20{%20json%20}%20sectionsCollection%20{%20items%20{%20title%20description%20link%20linkText%20image%20{%20url%20width%20height%20}%20sectionName%20resourcesCollection(limit:%20$limit)%20{%20items%20{%20eyebrow%20title%20description%20externalLink%20externalLinkText%20image%20{%20url%20width%20height%20}%20}%20}%20}%20}%20}%20}%20&variables={%20%22contentDatesId%22:%20%22${id}%22,%20%22preview%22:%20${preview},%20%22limit%22:%206%20}`;
     fetch(contentURL)
       .then(response => response.json())
       .then(data => {
         this.setState((prevState) => {
-          console.log(data.data.contentDates);
           prevState.affirmation = data.data.contentDates.affirmation.json.content[0].content[0].value;
           prevState.content = data.data.contentDates.sectionsCollection.items;
           localStorage.setItem('state', JSON.stringify(prevState));
           return prevState;
         });
-      });
+      })
   }
 
   saveStateWithoutCustomization(param, val) {
     this.setState(prevState => {
-      console.log(param, val);
       prevState[param]= val;
       prevState.isCustomizing = false;
       localStorage.setItem('state', JSON.stringify(prevState));
